c71e1b4a93f30546c994d363b2bb263749f90512
Liam <paledeepened@gmail.com>
2023-12-09 15:07:25 +0000

    Task 3 of milestone 4 complete.

--- milestone_4.py
+++ milestone_4.py
@@ -8,10 +8,32 @@ class Hangman():
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.list_of_guesses = []
+        
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print( f"Good guess! {guess} is in the word.")
+            for letter in enumerate(self.word):
+                if letter[1] == guess:
+                    self.word_guessed[letter[0]] = guess
+            self.num_letters -= 1
+            print(self.word_guessed)
+        else:
+            pass
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            if len(guess) != 1 and guess.isalpha() == False:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                continue
+            elif guess in self.list_of_guesses:
+                    print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
 
 if __name__ == '__main__':
     fruit_list = ['apple', 'banana', 'grape', 'kiwi', 'strawberry']
     game = Hangman(fruit_list)
-    print(game.word)
-    print(game.num_letters)
-    print(game.word_guessed)
+    game.ask_for_input()
