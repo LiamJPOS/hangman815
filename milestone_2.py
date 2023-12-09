5c290b091214b0d0007a1b138910a8c1828d4413
Liam <paledeepened@gmail.com>
2023-12-09 11:13:34 +0000

    Optimised code based on progress from milestone 3.

--- milestone_2.py
+++ milestone_2.py
@@ -5,14 +5,25 @@ class Hangman():
         self.word_list = ['apple', 'banana', 'grape', 'kiwi', 'strawberry']
         self.word = random.choice(self.word_list)
     
-    def guess_letter(self):
-        while True:
+    def check_guess(self, guess):
+        '''Checks if user input is in the word to be guessed.''' 
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+                
+    def ask_for_input(self):      
+        '''Asks user for input and checks if input is a single alphabetical character. It then runs check_guess'''
+        while True:   
             guess = input('Guess a letter: ')
-            if guess != 1 and guess.isalpha() == False:
-                print('Oops that\'s not a valid input')
-                continue
-            else:
-                print('Good guess')
+            if guess.isalpha():
                 break
-            
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                continue
+        self.check_guess(guess)
 
+if __name__ == "__main__":
+    new_game = Hangman()
+    new_game.ask_for_input()
